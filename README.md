f08c3d430931ecf25dbd02085673c62b0b656eb5
Einar Thorsrud <einar.thorsrud@nordicsemi.no>
2020-09-15 10:30:42 +0200

    Implemented basic CC310 KDR key derivatino sample.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # nrf52_cc310_kdr_example
 
-Example provisioning CC310 with the device root key (KDR) and then using key derivatin to extract the key and perform AES crypto operations.
+Minimal example provisioning CC310 with the device root key (KDR) and then using key derivatin to extract a key.
+
+Tested using nRF5 SDK 17.0.2, though the code itself should work with any nRF5 SDK version >= 15.0.0.
--- main.c
+++ main.c
@@ -49,333 +49,147 @@
 #include "integration_test_plat_defs.h"
 #include "integration_test_ssi_data.h"
 #include "integration_test_ssi_defs.h"
+#include "ssi_util_key_derivation.h"
+#include "ssi_util_error.h"
 
-/*AES test data vectors*/
-extern aesDataStuct aesVectors[];
-extern aesCCMDataStuct aesCCMVectors[];
 
 /*Globals*/
-extern CRYS_RND_State_t*     rndState_ptr;
-extern CRYS_RND_WorkBuff_t*  rndWorkBuff_ptr;
+extern CRYS_RND_State_t *rndState_ptr;
+extern CRYS_RND_WorkBuff_t *rndWorkBuff_ptr;
 
-int aes_ccm_tests(void)
+static bool is_kdr_set(void)
 {
-	uint32_t     ret = 1;
-	int test_index;
-	uint8_t dataInBuff[MAX_AES_CCM_DATA_SIZE_VECTOR];
-	uint8_t dataOutBuff[MAX_AES_CCM_DATA_SIZE_VECTOR];
-	uint8_t aDataBuff[MAX_AES_CCM_DATA_SIZE_VECTOR];
-	uint8_t macOutRes[MAX_AES_CCM_DATA_SIZE_VECTOR];
-	uint8_t nonce[MAX_AES_CCM_DATA_SIZE_VECTOR];
-
-	for (test_index = 0;test_index < AES_CCM_TESTS_NUMBER ;test_index++)
-	{
-
-        SaSi_PalMemSetZero(dataInBuff,MAX_AES_CCM_DATA_SIZE_VECTOR);
-        SaSi_PalMemSetZero(dataOutBuff,MAX_AES_CCM_DATA_SIZE_VECTOR);
-        SaSi_PalMemSetZero(aDataBuff,MAX_AES_CCM_DATA_SIZE_VECTOR);
-        SaSi_PalMemSetZero(macOutRes,MAX_AES_CCM_DATA_SIZE_VECTOR);
-        SaSi_PalMemSetZero(nonce,MAX_AES_CCM_DATA_SIZE_VECTOR);
-		SaSi_PalMemCopy(dataInBuff, aesCCMVectors[test_index].aesCCMTest_TextData, aesCCMVectors[test_index].aesCCMTest_TextdataSize);
-		/*Copy input additional data to input buffer*/
-		SaSi_PalMemCopy(aDataBuff, aesCCMVectors[test_index].aesCCMTest_AddData, aesCCMVectors[test_index].aesCCMTest_AddDataSize);
-		/*Copy nonce data to nonce buffer*/
-		SaSi_PalMemCopy(nonce, aesCCMVectors[test_index].aesCCMTest_NData, aesCCMVectors[test_index].aesCCMTest_NDataSize /*7*/);
-
-
-
-		INTEG_TEST_PRINT(" Test Parameters : %s \n",(uint32_t)aesCCMVectors[test_index].aesCCMTest_name);
-
-		/*Call CRYS_AESCCM integrated function*/
-		ret = CRYS_AESCCM(SASI_AES_ENCRYPT,
-			aesCCMVectors[test_index].aesCCMTest_Key,
-			aesCCMVectors[test_index].aesCCMTest_KeySize,
-			nonce,//aesCCMVectors[test_index].aesCCMTest_NData,
-			aesCCMVectors[test_index].aesCCMTest_NDataSize,
-			aDataBuff,//aesCCMVectors[test_index].aesCCMTest_AddData,,
-			aesCCMVectors[test_index].aesCCMTest_AddDataSize,
-			dataInBuff,//aesCCMVectors[test_index].aesCCMTest_TextData,
-			aesCCMVectors[test_index].aesCCMTest_TextdataSize,
-			dataOutBuff,//aesCCMVectors[test_index]  uint8_t *TextDataOut_ptr,
-			aesCCMVectors[test_index].aesCCMTest_Output_CCM_MACSize,
-			macOutRes /*aesCCMVectors[test_index]  CRYS_AESCCM_Mac_Res_t MacRes*/);
-
-		if (ret != SA_SILIB_RET_OK){
-			INTEG_TEST_PRINT(" CRYS_AESCCM failed with 0x%x \n",ret);
-			goto aesTester_end;
-		}
-		/*Check output text data with expected data*/
-		ret = SaSi_PalMemCmp(dataOutBuff,aesCCMVectors[test_index].aesCCMTest_Expected_output_data,aesCCMVectors[test_index].aesCCMTest_TextdataSize);
-		if (ret != 0){
-			INTEG_TEST_PRINT("output text buffer of the test failed");
-			goto aesTester_end;
-		}
-		/*Check mac output with expected data*/
-		ret = SaSi_PalMemCmp(macOutRes,aesCCMVectors[test_index].aesCCMTest_Expected_MAC_output_data,aesCCMVectors[test_index].aesCCMTest_Output_CCM_MACSize);
-		if (ret != 0){
-			INTEG_TEST_PRINT("output mac buffer of the test failed");
-			goto aesTester_end;
-		}
-
-	}
-	INTEG_TEST_PRINT("Passed \n");
-aesTester_end:
-	return ret;
-}
-
-
-
-
-
-static bool should_set_iv(SaSiAesOperationMode_t  	operationMode)
-{
-    return (operationMode == SASI_AES_MODE_CBC) ||
-	     (operationMode == SASI_AES_MODE_CTR) ||
-	     (operationMode == SASI_AES_MODE_CBC_MAC);
-}
-
-int aes_tests(void){
-
-	int ret = 0;
-	uint32_t test_index,block_index;
-	BufferType_t buffer_index;
-	uint8_t dataInBuff[MAX_TEST_DATA_SIZE];
-	uint8_t dataOutBuff[MAX_TEST_DATA_SIZE];
-    uint8_t *dataOutBuff_ptr = dataOutBuff;
-	SaSiAesUserContext_t    ContextID;
-	SaSiAesUserKeyData_t    keyData;
-    size_t  outSize = 0;
-	/*Loop for aes test vectors*/
-	for (buffer_index = NON_INPLACE_BUFFER ; buffer_index <= INPLACE_BUFFER; buffer_index++)
-		for (test_index = 0; test_index < AES_TESTS_NUMBER; test_index++)
-		{
-
-
-            SaSi_PalMemSetZero(dataInBuff,MAX_TEST_DATA_SIZE);
-            SaSi_PalMemSetZero(dataOutBuff,MAX_TEST_DATA_SIZE);
-			SaSi_PalMemCopy(dataInBuff, aesVectors[test_index].aesTest_input_data, aesVectors[test_index].aesTest_DataSize);
-
-			INTEG_TEST_PRINT(" Test Parameters : %s \n",(uint32_t)aesVectors[test_index].aesTest_name);
-
-			/*If current iteration is for inplace buffer type, set the output data pointer to input data pointer*
-			* to check inplace operation      						      */
-			if (buffer_index == INPLACE_BUFFER ){
-				/*Perform inplace operation*/
-				dataOutBuff_ptr = dataInBuff;
-				INTEG_TEST_PRINT(" Inplace buffer operaion \n");
-			} else {
-				INTEG_TEST_PRINT(" Non-Inplace buffer operaion \n");
-			}
-
-
-
-   		/*Call non-integrated APIs - first SaSi_AesInit*/
-   		ret = SaSi_AesInit(&ContextID,
-   			aesVectors[test_index].aesTest_EncDecMode,
-   			aesVectors[test_index].aesTest_OperationMode,SASI_AES_PADDING_NONE);
-
-        /*,
-            aesVectors[test_index].aesTest_Key,   			aesVectors[test_index].aesTest_KeySize,*/
-
-   		if (ret != SA_SILIB_RET_OK){
-   			INTEG_TEST_PRINT("CRYS_AES_Init failed with error 0x%x\n",ret);
-   			return ret;
-   		}
-        if ( should_set_iv(aesVectors[test_index].aesTest_OperationMode)){
-            ret = SaSi_AesSetIv(&ContextID, aesVectors[test_index].aesTest_IvCounter);
-            if (ret != SA_SILIB_RET_OK){
-       			INTEG_TEST_PRINT("SaSi_AesSetIv failed with error 0x%x\n",ret);
-       			return ret;
-       		}
-        }
-        keyData.pKey = aesVectors[test_index].TST_KeyData;
-        keyData.keySize = aesVectors[test_index].TST_KeySize;
-        ret = SaSi_AesSetKey(&ContextID, SASI_AES_USER_KEY, &keyData, sizeof(keyData) );
-        if (ret != SA_SILIB_RET_OK){
-   			INTEG_TEST_PRINT("SaSi_AesSetKey failed with error 0x%x\n",ret);
-   			return ret;
-   		}
-   		/*Call CRYS_AES_Block for each 16 byte block data of the input buffer*/
-   		for (block_index = 0; block_index<aesVectors[test_index].aesTest_DataSize/SASI_AES_BLOCK_SIZE_IN_BYTES-1; block_index++)
-   		{
-   			ret = SaSi_AesBlock(&ContextID,
-   				dataInBuff + (block_index*SASI_AES_BLOCK_SIZE_IN_BYTES),
-   				SASI_AES_BLOCK_SIZE_IN_BYTES,
-   				dataOutBuff_ptr + (block_index*SASI_AES_BLOCK_SIZE_IN_BYTES));
-
-   			if (ret != SA_SILIB_RET_OK){
-   				INTEG_TEST_PRINT("CRYS_AES_Block  failed with error 0x%x\n",ret);
-   				return ret;
-   			}
-   		}
-        outSize = aesVectors[test_index].aesTest_DataSize - (block_index*SASI_AES_BLOCK_SIZE_IN_BYTES);
-        /*Call CRYS_AES_Finish for the last block (the size may be 0)*/
-        if (aesVectors[test_index].aesTest_OperationMode == SASI_AES_MODE_XCBC_MAC ||
-            aesVectors[test_index].aesTest_OperationMode == SASI_AES_MODE_CMAC){
-			/*For XCBC_MAC and CMAC modes we should pass to SaSi_AesFinish the begining of the output buffer*/
-					ret = SaSi_AesFinish(&ContextID,
-						outSize,
-						dataInBuff + (block_index*SASI_AES_BLOCK_SIZE_IN_BYTES),
-						outSize,
-						dataOutBuff_ptr,
-						&outSize);
-
-
-				} else {
-					/*For the rest of the modes we should pass to SaSi_AesFinish the relevant offset of output buffer*/
-					ret = SaSi_AesFinish(&ContextID,
-						outSize,
-						dataInBuff + (block_index*SASI_AES_BLOCK_SIZE_IN_BYTES),
-						outSize,
-						dataOutBuff_ptr + (block_index*SASI_AES_BLOCK_SIZE_IN_BYTES),
-						&outSize);
-                }
-
-
-   		if (ret != SA_SILIB_RET_OK){
-   				INTEG_TEST_PRINT("CRYS_AES_Finish  failed with error 0x%x\n",ret);
-   				return ret;
-   		}
-
-
-   	/*Check the output buffer with expected data */
-   	ret = SaSi_PalMemCmp(dataOutBuff_ptr,aesVectors[test_index].aesTest_Expected_output_data,aesVectors[test_index].aesTest_OutputDataSize);
-   	if (ret != 0){
-   		INTEG_TEST_PRINT("output buffer of the test failed\n");
-   		return ret;
-   	}
-   	INTEG_TEST_PRINT(" Passed \n");
-	}
-	return ret;
-}
-
-#ifdef DX_LINUX_PLATFORM /*for linux platform only  -> we need to use contiguous memory for stack !!*/
-/*aes_ccm_wrap_tests creates thread with defined stack address to and calls to AES CCM test */
-void* aes_thread(void)
-{
-   uint32_t* threadReturnValue = SaSi_PalMemMalloc(sizeof(uint32_t));
-   *threadReturnValue =aes_tests();
-    if (*threadReturnValue != SA_SILIB_RET_OK) {
-        INTEG_TEST_PRINT("Failure in aes_tests,ret = 0x%x\n", *threadReturnValue);
-        goto exit;
+    if ((NRF_CC_HOST_RGF->HOST_IOT_KDR0 == 1) &&
+        (NRF_CC_HOST_RGF->HOST_IOT_KDR0 == 1) &&
+        (NRF_CC_HOST_RGF->HOST_IOT_KDR0 == 1) &&
+        (NRF_CC_HOST_RGF->HOST_IOT_KDR0 == 1))
+    {
+        return true;
     }
-	/*Call chacha poly test*/
-
-	*threadReturnValue = aes_ccm_tests();
-	if (*threadReturnValue != SA_SILIB_RET_OK) {
-	    INTEG_TEST_PRINT("Failure in aes_ccm_tests,ret = 0x%x\n", *threadReturnValue);
-	    goto exit;
-	}
-
-exit:
-	pthread_exit(threadReturnValue);
 }
 
-int aes_wrap_tests(void){
-	uint32_t rc = 0;
+static void set_lcs_secure(void)
+{
+    /* Set life-cycle state to Secure (write once per reset) */
+    NRF_CC_HOST_RGF->HOST_IOT_LCS = CC_HOST_RGF_HOST_IOT_LCS_LCS_Secure << CC_HOST_RGF_HOST_IOT_LCS_LCS_Pos;
 
-	pthread_t threadId;
-	pthread_attr_t threadAttr;
-	int threadRc;
-	void *threadRet;
-	int num = 6;
+    /* Needed in order to delay until register is written before continuing to verify (next step) */
+    NRF_CC_HOST_RGF->HOST_IOT_LCS;
 
-	threadRc = pthread_attr_init(&threadAttr);
-	if (threadRc != 0) {
-		INTEG_TEST_PRINT("pthread_attr_init failed\n");
-		return -1;
-	}
+    /* Verify CLS set to Secure */
+    if (((NRF_CC_HOST_RGF->HOST_IOT_LCS & CC_HOST_RGF_HOST_IOT_LCS_LCS_IS_VALID_Msk) >> CC_HOST_RGF_HOST_IOT_LCS_LCS_IS_VALID_Pos) != CC_HOST_RGF_HOST_IOT_LCS_LCS_IS_VALID_Valid)
+    {
 
-	threadRc = pthread_attr_setstack(&threadAttr, g_test_stack_base_addr, PTHREAD_STACK_SIZE);
-	if (threadRc != 0) {
-		INTEG_TEST_PRINT("pthread_attr_setstack failed\n");
-		return -1;
-	}
+        INTEG_TEST_PRINT("LCS is invalid! \n");
+    }
 
-	/* Create independent thread which run with  */
-	threadRc = pthread_create( &threadId, &threadAttr, (void *)aes_thread, &num);
-	if (threadRc != 0) {
-		INTEG_TEST_PRINT( "pthread_create failed\n");
-		return -1;
-	}
+    if (((NRF_CC_HOST_RGF->HOST_IOT_LCS & CC_HOST_RGF_HOST_IOT_LCS_LCS_Msk) >> CC_HOST_RGF_HOST_IOT_LCS_LCS_Pos) != CC_HOST_RGF_HOST_IOT_LCS_LCS_Secure)
+    {
 
-	/* Wait till thread is complete before main continues */
-	threadRc = pthread_join( threadId, &threadRet);
-	if (threadRc != 0) {
-		INTEG_TEST_PRINT( "pthread_join failed\n");
-		return -1;
-	}
-
-	rc =*((uint32_t *)*&threadRet);
-
-	SaSi_PalMemFree(threadRet);
-	threadRc = pthread_attr_destroy(&threadAttr);
-	if (threadRc != 0) {
-		INTEG_TEST_PRINT("pthread_attr_destroy failed\n");
-	}
-
-	return rc;
+        INTEG_TEST_PRINT("LCS not secure! \n");
+    }
 }
-#endif
+
+
+static void set_kdr(void)
+{
+    /* Enable the CC310 HW (needed to set KDR) */
+    NRF_CRYPTOCELL->ENABLE = 1;
+
+    /*
+    The following tasks could be done here to achieve a higher degree of trust, but are not demonstrated:
+
+    * Secure configuration area should be authenticated and decrypted using CRYPTOCELL KPRTL key.
+      See Example: Data at rest for more information.
+
+    * After reading the secure configuration area from flash to SRAM, and successfully
+      authenticating and decrypting its content, KPRTL key must be locked from use until next reset
+      by writing register CC_HOST_RGF->HOST_IOT_KPRTL_LOCK
+    */
+
+    /* Set Life cycle state (LCS) to Secure */
+    set_lcs_secure();
+
+    /* Set KDR */
+    NRF_CC_HOST_RGF->HOST_IOT_KDR0 = 0xBADEBA11;
+    NRF_CC_HOST_RGF->HOST_IOT_KDR1 = 0xBADEBA11;
+    NRF_CC_HOST_RGF->HOST_IOT_KDR2 = 0xBADEBA11;
+    NRF_CC_HOST_RGF->HOST_IOT_KDR3 = 0xBADEBA11;
+
+    /* Verify that KDR is set */
+    if (!is_kdr_set())
+    {
+        INTEG_TEST_PRINT("KDR not set correctly\n");
+    }
+
+    /* Disable the CC310 HW */
+    //NRF_CRYPTOCELL->ENABLE = 0;
+}
+
+
+static void key_derivation(void)
+{
+    SaSiUtilError_t err;
+
+    /* Derive keys according to NIST SP800-108, where
+       AES-CMAC is used as pseudo-random function */
+
+    /* Key labels, max allowed size of 64 bytes */
+    const uint8_t key_label[11] = {"KEY ENC KEY"};
+
+    /* Context for above key labels, max allowed size of 64 bytes.
+       This example use the device ID which is unique for each IC. */
+    const uint32_t key_context[2] = {NRF_FICR->DEVICEID[0],
+                                     NRF_FICR->DEVICEID[1]};
+
+    /* Array in SRAM for holding derived key value once complete */
+    uint8_t secret_key[16] = {0};
+
+    /* Derive external storage key */
+    err = SaSi_UtilKeyDerivation(SASI_UTIL_ROOT_KEY,
+                                 NULL,
+                                 key_label,
+                                 sizeof(key_label),
+                                 (const uint8_t *)key_context,
+                                 sizeof(key_context),
+                                 secret_key,
+                                 sizeof(secret_key));
+    if (err != SASI_UTIL_OK)
+    {  
+        INTEG_TEST_PRINT("Error wile deriving key. Err code: %u\n", err);
+    }
+    else
+    {
+        INTEG_TEST_PRINT("Successfully derived key \n");
+    }
+
+    /* Do crypto operation with the key here, now just dumping the key instead... */
+
+
+    /* Secret_key no longer needed, so we clear the memory holding the key */
+    SaSi_PalMemSetZero(&secret_key, sizeof(secret_key));
+}
 
 
 int main(void)
 {
-	int ret = 0;
+    int ret = 0;
 
-	/*Perform memory mapping*/
-	ret = integration_tests_setup();
-	if (ret != 0)
-	{
-		INTEG_TEST_PRINT("integration_tests_setup failed\n");
-		return ret;
-	}
-INTEG_TEST_PRINT("****************MAIN***********************\n");
-        /*Init SaSi library*/
-    ret = SaSi_LibInit();
-	if (ret != SA_SILIB_RET_OK) {
-        INTEG_TEST_PRINT("Failed SaSi_LibInit - ret = 0x%x\n", ret);
-	    goto exit_1;
-	}
-
-    ret = CRYS_RndInit(rndState_ptr, rndWorkBuff_ptr);
-    if (ret != SA_SILIB_RET_OK) {
-        INTEG_TEST_PRINT("Failed CRYS_RndInit - ret = 0x%x\n", ret);
-        goto exit_1;
+    /* Setup logging etc */
+    ret = integration_tests_setup();
+    if (ret != 0)
+    {
+        INTEG_TEST_PRINT("integration_tests_setup failed\n");
+        return ret;
     }
 
-	/*Call aes test*/
-#ifdef DX_LINUX_PLATFORM
-	ret = aes_wrap_tests(); /*Call wrap function to create thread and to define stack's address to use contiguous memory*/
-#else
-        /*Call aes test*/
-	ret = aes_tests();
-	if (ret != SA_SILIB_RET_OK) {
-	    INTEG_TEST_PRINT("Failure in aes test,ret = 0x%x\n", ret);
-	    goto exit_0;
-	}
-	ret = aes_ccm_tests();
-#endif
-	if (ret != SA_SILIB_RET_OK) {
-	    INTEG_TEST_PRINT("Failure in aes ccm test,ret = 0x%x\n", ret);
-	    goto exit_0;
-	}
+    /* Set root key and set secure mode. This would typically be done in the bootloader and the page
+       holding the root key should be protected using ACL */
+    set_kdr();
 
-	INTEG_TEST_PRINT("All tests passed \n");
+    /* Get the derived key. This would typically be done in the application where the key is needed
+       for crypto operations. */
+    key_derivation();
 
-exit_0:
-	/*Finish SaSi library*/
-    SaSi_LibFini();
-
-    ret = CRYS_RND_UnInstantiation(rndState_ptr);
-
-    if (ret) {
-        INTEG_TEST_PRINT("Failure in CRYS_RND_UnInstantiation,ret = 0x%x\n", ret);
-    }
-exit_1:
-	integration_tests_clear();
-
-	return ret;
+    return ret;
 }
-
-
